a728921421eef00d50293cc75eaf065076756088
Amirhan0 <amirgasanov246@gmail.com>
2024-10-30 19:30:42 +0600

    upd: localstorage

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -6,8 +6,55 @@ function MapCanvas() {
   const canvasRef = useRef(null);
   const [figurePosition, setFigurePosition] = useState({ x: 0, y: 0 });
   const [points, setPoints] = useState(() => {
-    const savedPoints = localStorage.getItem("points");
-    return savedPoints ? JSON.parse(savedPoints) : []; 
+    const initialPoints = [
+      { x: 237, y: 530 },
+      { x: 241, y: 556 },
+      { x: 243, y: 584 },
+      { x: 228, y: 610 },
+      { x: 195, y: 628 },
+      { x: 167, y: 637 },
+      { x: 134, y: 628 },
+      { x: 117, y: 601 },
+      { x: 107, y: 576 },
+      { x: 100, y: 554 },
+      { x: 97, y: 527 },
+      { x: 96, y: 500 },
+      { x: 108, y: 475 },
+      { x: 129, y: 459 },
+      { x: 153, y: 441 },
+      { x: 174, y: 422 },
+      { x: 198, y: 409 },
+      { x: 210, y: 385 },
+      { x: 208, y: 358 },
+      { x: 195, y: 337 },
+      { x: 173, y: 315 },
+      { x: 138, y: 297 },
+      { x: 110, y: 290 },
+      { x: 86, y: 287 },
+      { x: 60, y: 274 },
+      { x: 51, y: 258 },
+      { x: 65, y: 243 },
+      { x: 104, y: 252 },
+      { x: 133, y: 254 },
+      { x: 152, y: 254 },
+      { x: 176, y: 249 },
+      { x: 192, y: 241 },
+      { x: 208, y: 232 },
+      { x: 225, y: 223 },
+      { x: 233, y: 206 },
+      { x: 235, y: 193 },
+      { x: 224, y: 179 },
+      { x: 211, y: 167 },
+      { x: 201, y: 154 },
+      { x: 191, y: 142 },
+      { x: 187, y: 131 },
+      { x: 185, y: 110 },
+      { x: 185, y: 85 },
+      { x: 181, y: 68 }
+    ];
+
+    localStorage.setItem("points", JSON.stringify(initialPoints));
+    return initialPoints; 
   });
 
   const drawCanvas = () => {
@@ -34,7 +81,6 @@ function MapCanvas() {
         context.fillStyle = "blue";
         context.fill();
       });
-
       figure.onload = () => {
         context.drawImage(figure, figurePosition.x - 15, figurePosition.y - 15, 30, 30);
       };
@@ -65,12 +111,6 @@ function MapCanvas() {
         pointClicked = true; 
       }
     });
-
-    if (!pointClicked) {
-      const newPoints = [...points, { x, y }];
-      setPoints(newPoints);
-      localStorage.setItem("points", JSON.stringify(newPoints)); 
-    }
   };
 
   return (
